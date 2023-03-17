41cd964fce26f07a24863c2564b9e447b124838d
shmt <shmt25@proton.me>
2023-03-17 08:24:46 +0100

    begin work on the lexer

--- src/main.v
+++ src/main.v
@@ -1,5 +1,39 @@
 module main
 
+import cli { Command, Flag }
+import os
+
 fn main() {
-	println('Hello World!')
+	mut cmd := Command{
+		name: 'rap'
+		description: 'Rapid and Painless programming language.'
+		version: '1.0.0'
+		usage: '<file>'
+		required_args: 1
+		pre_execute: run_pre_func
+		execute: run_func
+	}
+	mut run_cmd := Command{
+		name: 'run'
+		description: 'Runs a .rap file'
+		usage: '<file>'
+		required_args: 1
+		pre_execute: run_pre_func
+		execute: run_func
+	}
+	cmd.add_command(run_cmd)
+	cmd.setup()
+	cmd.parse(os.args)
+}
+
+fn run_func(cmd Command) ! {
+	file := cmd.args[0]
+}
+
+fn run_pre_func(cmd Command) ! {
+	file := cmd.args[0]
+  if !os.exists(file) {
+    println("==> No file with the name '${file}' <==")
+    exit(1)
+  }
 }
